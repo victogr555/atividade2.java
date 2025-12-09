b3c9ebda67cb86fb22a2b4355c4797bd350fea8c
victogr555 <victogr25@gmail.com>
2025-12-09 10:36:35 -0300

    .

--- src/Questão3/TestUser.java
+++ src/Questão3/TestUser.java
@@ -5,12 +5,12 @@ public class TestUser {
 
         User user1 = new User("João", "joao@email.com");
         User user2 = new User("Maria", "maria@email.com");
-        User user3 = new User("Pedro", "pedro.email.com"); // email inválido
-
+        User user3 = new User("Pedro", "pedro.email.com");
         System.out.println("Total de usuários: " + User.getTotalUsers());
         System.out.println("User1 ID: " + user1.getId());
         System.out.println("Email válido? " + User.isValidEmail("teste@email.com"));
     }
 }
+
 
 
